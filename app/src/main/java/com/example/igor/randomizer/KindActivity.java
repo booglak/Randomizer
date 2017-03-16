ccfe2843e8841a7d024ffc338f9c09ff93f64576
I_Buglak <i.buglak@nullgr.com>
2017-03-16 19:08:34 +0200

    start Activity Done

--- app/src/main/java/com/example/igor/randomizer/KindActivity.java
+++ app/src/main/java/com/example/igor/randomizer/KindActivity.java
@@ -2,12 +2,39 @@ package com.example.igor.randomizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class KindActivity extends AppCompatActivity {
+public class KindActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static boolean isNumeric;
+    ImageButton btnNumeric;
+    ImageButton btnObjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kind);
+
+        btnNumeric = (ImageButton)findViewById(R.id.ibtnNumeric);
+        btnObjects = (ImageButton)findViewById(R.id.ibtnObjects);
+
+        btnNumeric.setOnClickListener(this);
+        btnObjects.setOnClickListener(this);
+    }
+
+    public static boolean getNumeric(){
+        return isNumeric;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.ibtnNumeric:
+                break;
+            case R.id.ibtnObjects:
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/igor/randomizer/StartActivity.java
+++ app/src/main/java/com/example/igor/randomizer/StartActivity.java
@@ -1,13 +1,38 @@
 package com.example.igor.randomizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvStart;
+    TextView tvExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        tvStart = (TextView) findViewById(R.id.tvStart);
+        tvExit = (TextView) findViewById(R.id.tvExit);
+
+        tvStart.setOnClickListener(this);
+        tvExit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvStart:
+                Intent intent = new Intent(this, KindActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tvExit:
+                finish();
+                break;
+        }
     }
 }
