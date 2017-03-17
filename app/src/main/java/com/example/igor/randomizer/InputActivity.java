b8822163444b94b4383d84b678da85892d3310e8
I_Buglak <i.buglak@nullgr.com>
2017-03-17 18:23:31 +0200

    InputActivity develop

--- app/src/main/java/com/example/igor/randomizer/InputActivity.java
+++ app/src/main/java/com/example/igor/randomizer/InputActivity.java
@@ -1,8 +1,11 @@
 package com.example.igor.randomizer;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -54,6 +57,7 @@ public class InputActivity extends AppCompatActivity implements View.OnClickList
                 }
                 System.out.println(randomList.size());
                 break;
+
             case R.id.btnRandom:
                 break;
         }
@@ -64,5 +68,25 @@ public class InputActivity extends AppCompatActivity implements View.OnClickList
     public void onBackPressed(){
         super.onBackPressed();
         randomList.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_input, menu);
+        return true;
+    }
+
+    public void onMenuListClick(){
+
+
+        String[] items = new String[randomList.size()];
+        for (int i = 0; i < randomList.size(); i++) {
+            items[i] = randomList.get(i).toString();
+        }
+
+        AlertDialog.Builder listResult = new AlertDialog.Builder(this);
+        listResult.setTitle("Список");
+        listResult.setItems(items, (DialogInterface.OnClickListener) this);
+
     }
 }
