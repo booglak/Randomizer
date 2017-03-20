fc2c9b0f061db05f1eae429a90a6461eacdba293
I_Buglak <i.buglak@nullgr.com>
2017-03-20 16:54:59 +0200

    input context_menu input top_menu DONE

--- app/src/main/java/com/example/igor/randomizer/InputActivity.java
+++ app/src/main/java/com/example/igor/randomizer/InputActivity.java
@@ -6,6 +6,8 @@ import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +26,10 @@ public class InputActivity extends AppCompatActivity implements View.OnClickList
     Button btnRandom;
     ArrayList randomList;
 
+    //Пункты контекстного меню
+    public static final int CON_MEN_RANGE = 101;
+    public static final int CON_MEN_NON = 102;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +38,8 @@ public class InputActivity extends AppCompatActivity implements View.OnClickList
         etInput = (EditText)findViewById(R.id.etInput);
         btnAdd = (Button)findViewById(R.id.btnAdd);
         btnRandom = (Button)findViewById(R.id.btnRandom);
+
+        registerForContextMenu(etInput);
 
         btnAdd.setOnClickListener(this);
         btnRandom.setOnClickListener(this);
@@ -75,13 +83,14 @@ public class InputActivity extends AppCompatActivity implements View.OnClickList
         randomList.clear();
     }
 
+    //Создание верхнего меню
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_input, menu);
         return true;
     }
 
-
+    //Обработчик верхнего меню
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // получим идентификатор выбранного пункта меню
@@ -101,7 +110,6 @@ public class InputActivity extends AppCompatActivity implements View.OnClickList
                 builder.setItems(items, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int item) {
-                        // TODO Auto-generated method stub
                         Toast.makeText(getApplicationContext(),
                                 "Нажми RANDOM",
                                 Toast.LENGTH_SHORT).show();
@@ -113,6 +121,72 @@ public class InputActivity extends AppCompatActivity implements View.OnClickList
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //Создаем контекстное меню
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if (KindActivity.getNumeric()) {
+            menu.add(Menu.NONE, CON_MEN_RANGE, Menu.NONE, "Ввести диапазон");
+        }
+        else
+        menu.add(Menu.NONE, CON_MEN_NON, Menu.NONE, "Диапазон не доступен");
+    }
+
+    //Обработчик контекстного меню
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case CON_MEN_RANGE:
+
+                //Берем диалог из лейаута
+                LayoutInflater li = LayoutInflater.from(this);
+                View rangeDialog = li.inflate(R.layout.range_input, null);
+
+                AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(this);
+                mDialogBuilder.setView(rangeDialog);
+
+                final EditText userInputFrom = (EditText) rangeDialog.findViewById(R.id.etRangeFrom);
+                final EditText userInputTo = (EditText) rangeDialog.findViewById(R.id.etRangeTo);
+
+                mDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+
+                                        if (Integer.parseInt(userInputFrom.getText().toString()) > Integer.parseInt(userInputTo.getText().toString())){
+                                            Toast toast1 = Toast.makeText(getApplicationContext(),
+                                                    "Только в порядке возрастания", Toast.LENGTH_SHORT);
+                                            toast1.show();
+                                        }else
+
+                                        for (int i = Integer.parseInt(userInputFrom.getText().toString()); i < Integer.parseInt(userInputTo.getText().toString())+1; i++){
+                                            randomList.add(i);
+                                        }
+
+
+                                    }
+                                })
+                        .setNegativeButton("Отмена",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alertDialog = mDialogBuilder.create();
+                alertDialog.show();
+
+
+
+
+
+                break;
+        }
+        return true;
     }
 
 
